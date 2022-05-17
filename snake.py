4afa3fd8b1dd90962b23405317319b96bb8bb260
nutty-nut <104892591+nutty-nut@users.noreply.github.com>
2022-05-17 23:14:09 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,1 +1,28 @@
+import pygame
 
+scrn_width, scrn_height = 500, 500
+
+pygame.init()
+window = pygame.display.set_mode((scrn_width, scrn_height))
+pygame.display.set_caption("Snake z przeszkodami")
+run=True
+
+while run:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+      
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP:
+        #kierunek w gore
+      elif event.key == pygame.K_DOWN:
+        #kierunek w dol
+      elif event.key == pygame.K_LEFT:
+        #kierunek w lewo
+      elif event.key == pygame.K_RIGHT:
+        #kierunek w prawo
+        
+        
+
+      
+    
