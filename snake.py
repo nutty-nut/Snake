af9d54989fcd37b157865039ac10f85a3eb07c0f
nutty-nut <104892591+nutty-nut@users.noreply.github.com>
2022-05-24 22:31:53 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,36 +2,41 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 """----------------------------
 Coś tam zrobiłam ale wymaga dopracowania XD, w każdym razie poczatek mamy
 wymiary kratki - 50x50px
 wielkosc okna 600x600px
 -----------------------------------"""
 class Apple():
-    def __init__(self, okno):
+    def __init__(self, okno, kratka):
         self.image = pygame.image.load("apple.png").convert()
         self.okno = okno
-        self.size = 50 
-        self.x, self.y = 50, 100 #polozenie jablka na planszy (wspolrzedne)
+        self.size = kratka
+        self.x, self.y = 5*self.size, 2*self.size #polozenie jablka na planszy (wspolrzedne)
 
     def draw(self):
         self.okno.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
 class Snake():
-    def __init__(self, okno, dlugosc):
+    def __init__(self, okno, dlugosc, kratka):
         self.dlugosc = dlugosc
         self.okno = okno
+        self.head = pygame.image.load("head.png").convert()
         self.block = pygame.image.load("klocek.png").convert()
-        self.size=50
-        self.x = [self.size]*dlugosc 
+        self.size= kratka
+        self.x = [self.size]*dlugosc
         self.y =[self.size]*dlugosc
         self.direction = "up"
 
     def draw(self):
         self.okno.fill((93,185,127))
         for i in range(self.dlugosc):
-            self.okno.blit(self.block, (self.x[i], self.y[i]))
+            if i==0:
+                self.okno.blit(self.head, (self.x[i], self.y[i]))
+            else:
+                self.okno.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def move(self, key): #nadaje kierunek wezowi
@@ -49,31 +54,32 @@ class Snake():
             self.x[i]=self.x[i-1]
             self.y[i]=self.y[i-1]
         if self.direction=="up":
-            self.y[0]-=50
+            self.y[0]-=self.size
             self.draw()
         elif self.direction=="down":
-            self.y[0]+=50
+            self.y[0]+=self.size
             self.draw()
         elif self.direction=="left":
-            self.x[0]-=50
+            self.x[0]-=self.size
             self.draw()
         elif self.direction=="right":
-            self.x[0]+=50
+            self.x[0]+=self.size
             self.draw()
-
 
 class Snakegame():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Snake z przeszkodami")
-        self.surface = pygame.display.set_mode((600, 600)) #rysuje powierzchnie do gry
-        self.surface.fill((93,185,127)) #kolorek, wybralam taki brzydki, ale to sie zmieni 
-        self.snake=Snake(self.surface,5) #idk, zeby bylo latwiej na razie snake jest z dlugoscia 5 zeby bylo widac jak dziala (gra w powijakach! xd0
+        self.scrn_width, self.scrn_height = 900, 600
+        self.surface = pygame.display.set_mode((self.scrn_width, self.scrn_height)) #rysuje powierzchnie do gry
+        self.surface.fill((93,185,127)) #kolorek, wybralam taki brzydki, ale to sie zmieni
+        self.kratka = 30
+        self.snake=Snake(self.surface,5,self.kratka) #idk, zeby bylo latwiej na razie snake jest z dlugoscia 5 zeby bylo widac jak dziala (gra w powijakach! xd0
         self.snake.draw()
-        self.apple = Apple(self.surface)
+        self.apple = Apple(self.surface, self.kratka)
         self.apple.draw()
         self.points=0
-        
+
     def eat_apple(self, x1, y1, x2, y2): #sprawdza czy snake zjadl jablko
         if x1==x2 and y1==y2:
             self.points+=1
@@ -81,6 +87,7 @@ class Snakegame():
             return True
         else:
             return False
+
 
     def run(self):
         game = True
@@ -99,10 +106,10 @@ class Snakegame():
                         self.snake.move("right")
                     elif event.key == K_LEFT:
                         self.snake.move("left")
-            if self.eat_apple(self.snake.x[0],self.snake.y[0],self.apple.x,self.apple.y): 
+            if self.eat_apple(self.snake.x[0],self.snake.y[0],self.apple.x,self.apple.y):
                 #czary mary, jablko zmienia pozycje (Trzeba zrobic randoma)
-                self.apple.x=500 #random.randint(0,550) - kratka ma 50! - ale jeszcze trzeba zrobić zeby losowalo co 50, bo dziwnie inaczej
-                self.apple.y=300
+                self.apple.x=random.randint(0,29)*self.kratka #random.randint(0,550) - kratka ma 50! - ale jeszcze trzeba zrobić zeby losowalo co 50, bo dziwnie inaczej
+                self.apple.y=random.randint(0,19)*self.kratka
             self.snake.walk()
             self.apple.draw()
             time.sleep(0.3)
@@ -110,9 +117,3 @@ class Snakegame():
 
 game = Snakegame()
 game.run()
-
-
-        
-
-      
-    
